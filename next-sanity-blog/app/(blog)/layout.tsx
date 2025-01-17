e79bf2f4fdbfffab2faff7b47591577c3e403ecf
suleman khan <159639439+sulemankhani@users.noreply.github.com>
2025-01-17 23:05:53 +0500

    Update layout.tsx

--- next-sanity-blog/app/(blog)/layout.tsx
+++ next-sanity-blog/app/(blog)/layout.tsx
@@ -17,12 +17,12 @@ import * as demo from "@/sanity/lib/demo";
 import { sanityFetch } from "@/sanity/lib/fetch";
 import { settingsQuery } from "@/sanity/lib/queries";
 import { resolveOpenGraphImage } from "@/sanity/lib/utils";
+import Link from "next/link";
 
 export async function generateMetadata(): Promise<Metadata> {
   const settings = await sanityFetch({
     query: settingsQuery,
-    // Metadata should never contain stega
-    stega: false,
+    stega: false, // Metadata should never contain stega
   });
   const title = settings?.title || demo.title;
   const description = settings?.description || demo.description;
@@ -68,7 +68,7 @@ export default async function RootLayout({
     <html lang="en" className={`${inter.variable} bg-white text-black`}>
       <body>
         <section className="min-h-screen">
-           <AlertBanner />
+          <AlertBanner />
           <main>{children}</main>
           <footer className="bg-accent-1 border-accent-2 border-t">
             <div className="container mx-auto px-5">
@@ -80,16 +80,15 @@ export default async function RootLayout({
               ) : (
                 <div className="flex flex-col items-center py-28 lg:flex-row">
                   <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
-                  Thanks For Visit it.
+                    Thanks For Visit it.
                   </h3>
                   <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
-                    <a
+                    <Link
                       href="/"
                       className="mx-3 mb-6 border border-black bg-black py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black lg:mb-0 lg:px-8"
                     >
-                     Back to home
-                    </a>
-                   
+                      Back to home
+                    </Link>
                   </div>
                 </div>
               )}
